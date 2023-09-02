051874b6e804cae06f3904e0270e6818886f3c1a
mohiqudraty <mohi.qudraty@gmail.com>
2023-09-02 21:44:14 +0600

    assignment done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+let default_id = 1000;
+
 // get all categories api here
 const getApi = async () => {
   const response = await fetch(
@@ -9,8 +11,8 @@ const getApi = async () => {
 };
 
 // set category with dynamic id here &&&&& handleVideo() call here on button click
-const tabContainer = document.getElementById("tab-container");
 const setCategory = (data) => {
+  const tabContainer = document.getElementById("tab-container");
   data.data.forEach((category) => {
     const div = document.createElement("div");
     div.innerHTML = `
@@ -21,15 +23,57 @@ const setCategory = (data) => {
 };
 
 // set all video cards with required information &&&&& and call isNoData/ not found function
-const handleVideo = async (categoryId) => {
+const handleVideo = async (categoryId = 1000, sortByViews = "") => {
+  default_id = categoryId;
   const response = await fetch(
     `https://openapi.programming-hero.com/api/videos/category/${categoryId}`
   );
   const data = await response.json();
-  isNoData(data);
+
+  let viewsData = data.data;
+  if (sortByViews) {
+    viewsData.sort((a, b) => {
+      return (
+        parseInt(b.others.views.replace("K", "")) -
+        parseInt(a.others.views.replace("K", ""))
+      );
+    });
+  }
+
+  isThereData(data);
+
+  // setPostTime(data, categoryId);
   const cardContainer = document.getElementById("card-container");
   cardContainer.innerHTML = "";
   data.data.forEach((video) => {
+    const time = Math.floor(video.others.posted_date);
+    // let time = 2629440;
+    let extraSec = time % 60;
+
+    let min = time - extraSec;
+    min = min / 60;
+
+    let extraMin = min % 60;
+
+    let hours = min - extraMin;
+    hours = hours / 60;
+
+    let extraHours = hours % 60;
+
+    let days = hours - extraHours;
+    days = days / 24;
+
+    let extraDays = days % 30;
+
+    let months = days - extraDays;
+    months = months / 30;
+
+    let extraMonths = months % 12;
+
+    let years = months - extraMonths;
+    years = years / 12;
+    // console.log(years);
+
     const blueBadge = video.authors[0].verified
       ? '<img src="./image/badge.svg" />'
       : "";
@@ -39,15 +83,23 @@ const handleVideo = async (categoryId) => {
   <img
    class="w-[312px] h-[200px] object-cover" src="${video.thumbnail}"
   />
-  <p class="text-xs text-white absolute right-3 bottom-4 bg-[#171717] py-1 px-1 ">${video.others.posted_date}</p>
+  <p class="text-xs text-white absolute right-3 bottom-4 bg-[#171717] py-1 px-1 ">${
+    years ? `${years}years ago` : months ? `${months} Months ago` : ""
+  }
+  ${!years && days ? `${days} Days ago` : ""}
+  ${!years && extraHours > 0 ? `${extraHours} Hours` : ""}
+  ${!years && extraMin > 0 ? `${extraMin} Min ago` : ""}
+  </p>
 </figure>
 <div class="p-4">
 <div class="flex gap-3">
-<img class="w-10 h-10 rounded-full"  src="${video.authors[0].profile_picture}" />
+<img class="w-10 h-10 rounded-full"  src="${
+      video.authors[0].profile_picture
+    }" />
 <h2 class="text-base font-bold">${video.title}</h2>
 </div>
 
-<div class="pl-[50px]" > 
+<div class="pl-[50px]" >
 <div class="card-actions my-2">
 <div class="text-sm text-[#252525B2] ">${video.authors[0].profile_name}</div>
 ${blueBadge}
@@ -61,7 +113,7 @@ ${blueBadge}
 };
 
 // if no have elements than show not found
-const isNoData = (id) => {
+const isThereData = (id) => {
   const notFoundContainer = document.getElementById("not-found-container");
   notFoundContainer.innerHTML = "";
 
@@ -80,12 +132,8 @@ const isNoData = (id) => {
   notFoundContainer.appendChild(div);
 };
 
-const homePage = async () => {
-  const response = await fetch(
-    `https://openapi.programming-hero.com/api/videos/category/1000`
-  );
-  const data = await response.json();
-  handleVideo(1000);
+const homePage = () => {
+  handleVideo();
 };
 
 // button click to go blog page.
@@ -94,7 +142,11 @@ const readBlog = () => {
   window.location.href = "http://127.0.0.1:5500/blog.html";
 };
 
-// call function which element i want to show default in home page
-homePage();
+const sortByViews = () => {
+  handleVideo(default_id, default_id);
+};
 
+// call function which element i want to show default in home page
+
+homePage();
 getApi();
